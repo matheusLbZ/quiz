592491e64f4633ce9f54e277b4b0221e21f81b25
Matheus Bruno <matheusbrunno35@gmail.com>
2021-06-23 19:08:04 -0300

    Primeiro Commit

---
+++ assets/js/QandR.js
@@ -0,0 +1,28 @@
+let quiz = [
+    {
+        id: 0,
+        question: "Qual jogo ganhou a categoria game do ano em 2015?", 
+        response: ['Bloodborne', 'Fallout 4', 'Metal Gear Solid V: The Phantom Pain', 'The Witcher 3: Wild Hunt']
+    },
+    {
+        id: 1,
+        question: "Qual o nome da desenvolvedora de assassins creed?",
+        response: ['Cd Projeckt Red', 'Ubisoft', 'Rockstar Games', 'EA Games']
+    },
+    {  
+        id: 2,
+        question: "Qual o nome do vilão que era dono da rede de fast food Los Pollos Hermanos?",
+        response: ['Wilson Fisk', 'Arthur Fleck', 'Gustavo Fring', 'Ramsay Bolton']
+    },
+    {   
+        id: 3,
+        question: "Qual o filme com maior bilheteria da história dos cinemas?",
+        response: ['Vingadores: Ultimato', 'Senhor dos Aneis: O retorno do rei', 'Avatar', 'Vingadores Guerra Infinita']
+    },
+    {
+        id: 4,
+        question: "Qual o nome verdadeiro do Michael Kahnwald em DARK?",
+        response: ['Jonas Kahnwald', 'Mikkel Nielsen', 'Alfie Solomons', 'Kay Proctor']
+    },
+];
+
---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+const l = el => document.querySelector(el);
+const ls = el => document.querySelectorAll(el);
+
+
+function setModal() {
+    function cloneModal() {
+
+        quiz.map(() => {
+            let quizitem = l('.modal').cloneNode(true);
+
+            randomQuestions(quizitem);
+    
+            l('main').append(quizitem);
+        });    
+    };
+    cloneModal();
+
+    function randomQuestions(quizitem) {
+        const cart = [];
+
+        let search = quiz.filter(i=> i.id >= 0);
+        
+        while (cart.length < search.length) {
+            let random = Math.floor(Math.random() * search.length);
+
+            if (cart.indexOf(random) == -1) {
+                cart.push(random); 
+            };
+        };
+
+        for(let i in cart) {
+            let numbers = cart[i];
+            let resultNumbers = search[numbers];
+            console.log(resultNumbers);
+
+            quizitem.querySelectorAll('.modal-title strong').forEach(res => {
+                res.innerHTML = resultNumbers.question;
+            });
+    
+            quizitem.querySelectorAll('.modal-body .btn').forEach((res, index) => {
+                res.innerHTML = resultNumbers.response[index];
+            });
+        }
+
+    };
+};
+setModal();
+
+//Alternando de um modal para outro
+function numberId() {
+    for(let i = 0; i < 5; i++) {
+        ls('.modal').forEach(res=> {
+            res.setAttribute('id', `modalId${i+=1}`);
+        })
+    }
+
+    for(let f = 1; f < 5; f++) {
+        ls('.principal').forEach(res=> {
+            res.setAttribute('data-target', `#modalId${f+=1}`);
+        })
+    }
+};
+
+l('.btn1, .principal').addEventListener('click', numberId);
