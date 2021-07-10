abf90a737c5199fca80f19e74abdc4b844f15ddd
Matheus Bruno <matheusbrunno35@gmail.com>
2021-07-10 11:04:20 -0300

    correções

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,23 @@
 const l = el => document.querySelector(el);
 const ls = el => document.querySelectorAll(el);
 
-function setModal() {
-    function cloneModal() {
-        quiz.forEach(() => {
-            let quizitem = l('.modal').cloneNode(true);
-                console.log(quizitem)
-                randomQuestions(quizitem);
-
-            l('main').append(quizitem);
-        });    
-    };
-    cloneModal();
-
-    function randomQuestions(quizitem) {
-        const cart = [];
-
-        let search = quiz.filter(i=> i.id >= 0);
-        
-        while (cart.length < 1) {
-            let random = Math.floor(Math.random() * search.length);
-
-            if (cart.indexOf(random) == -1) {
-                cart.push(random);
-                
-            };
-        };
-
-        for(let i in cart) {
-            let numbers = cart[i];
-            
-            let resultNumbers = search[numbers]
-            console.log(resultNumbers) 
-
-            quizitem.querySelectorAll('.modal-title strong').forEach(res => {
-                res.innerHTML = resultNumbers.question;
-            });
+function cloneModal() {
+    quiz.forEach(item => {
+        let quizitem = l('.modal').cloneNode(true);
     
-            quizitem.querySelectorAll('.modal-body .btn').forEach((res, index) => {
-                res.innerHTML = resultNumbers.response[index];
-            });
-        };
-    };
+        quizitem.querySelectorAll('.modal-title strong').forEach(res => {
+            res.innerHTML = item.question;
+        });
+
+        quizitem.querySelectorAll('.modal-body .btn').forEach((res, index) => {
+            res.innerHTML = item.response[index];
+        });
+
+        l('main').appendChild(quizitem);
+        return quizitem;
+    }); 
 };
-setModal();
+cloneModal();
 
 //Alternando de um modal para outro
 function numberId() {
@@ -60,4 +34,4 @@ function numberId() {
     }
 };
 
-l('.btn1, .principal').addEventListener('click', numberId);
+l('.btn1, .principal').addEventListener('click', numberId);;
